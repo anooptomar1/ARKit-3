ab7514d750a42d49a9a6c14c9121d5fabc08adc6
zhangjiang1203 <896884553@qq.com>
2017-09-06 17:57:55 +0800

    初始化银河系AR模型

---
+++ ARKitTestDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ARKitTestDemo
+//
+//  Created by DFHZ on 2017/9/6.
+//  Copyright © 2017年 DFHZ. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        switch indexPath.row {
+        case 0:
+            let plateVC = MyARKitViewController.init()
+            self.navigationController?.pushViewController(plateVC, animated: true)
+        default:
+            print("还没有跳转事件哦")
+        }
+        
+    }
+
+
+}
+
